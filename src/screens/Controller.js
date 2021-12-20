32b780c23d3983b3e7be6e2b488f9ffd9e6c0224
anandchawla16 <74597089+anandchawla16@users.noreply.github.com>
2021-12-20 16:24:32 +0530

    Update Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://18.233.148.119:8080/api/v1/";
+    this.baseUrl = "http://52.90.191.246:8080/api/v1/";
     
   }
   render(){
